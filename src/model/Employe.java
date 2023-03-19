bde2ecfb178d3b2ab52d1386a7f4dbeb9fc59dd7
azzaimnisrine@gmail.com <azzaimnisrine@gmail.com>
2023-03-19 23:16:48 +0000

    second commit

--- src/model/Employe.java
+++ src/model/Employe.java
@@ -19,19 +19,19 @@ public class Employe {
     private Integer nbr_jrs_absence ;
 
     public String toString(){
-        var employeStr =  "==================================================         \n";
+        var employeStr =  "=========================================================         \n";
             employeStr += "=> Employé n° "+ getId_employe()+"                          \n";
             employeStr += "=> Nom de l'employé :"+getNom_employe()+"                 \n";
-        employeStr += "--------------------------------------------------         \n";
+        employeStr += "------------------------------------------------------          \n";
         employeStr += "=> Nom du poste          :"+getPoste()+"\n";
-        employeStr += "=> Nombre de jours du  travail                :"+getNbr_jrs_travail()+" jours \n";
-        employeStr += "=> Nombre de heures du  travail                :"+getNbr_hr_travail()+" heures \n";
+        employeStr += "=> Nombre de jours du  travail                :"+getNbr_jrs_travail()+" jours/semaine \n";
+        employeStr += "=> Nombre de heures du  travail                :"+getNbr_hr_travail()+" heures/jour \n";
         employeStr += "=> Prix de l'heure du  travail                :"+getPrix_heure()+" Dh \n";
-        employeStr += "=> Nombre de jours d'absence                 :"+getNbr_jrs_absence()+" jours \n";
-        employeStr += "--------------------------------------------------         \n";
+        employeStr += "=> Nombre de jours d'absence                 :"+getNbr_jrs_absence()+" jours/mois \n";
+        employeStr += "------------------------------------------------------         \n";
         employeStr += "=> Salaire                  :"
                 +(getSalaire() == 0.0 ? "NON-CALCULE ": getSalaire()+"Dh")+"\n";
-        employeStr += "==================================================         \n";
+        employeStr += "=========================================================         \n";
 
             return employeStr ;
     }
